77e829bd8f7252127ace92759517420d978a8008
Milan <milanvpage@gmail.com>
2021-03-29 23:39:14 -0700

    commit tree 73505d124688f9491eb315670a2f05bd0307b809

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,5 @@
 # Set up Bundler here following the instructions in the README
+#require_relative '../spec/bundler_spec.rb'
+#require 'awesome_print'
+require 'bundler/setup'
+Bundler.require(:default, :development)
